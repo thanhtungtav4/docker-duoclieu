100b2425eb5725e44df210421ee424103f2fa07c
thanhtungtav404 <nguyenthanhtung74119@gmail.com>
2023-03-30 17:54:53 +0700

    tungnt

--- wp_data/wp-content/themes/flatsome-child/functions.php
+++ wp_data/wp-content/themes/flatsome-child/functions.php
@@ -1,2 +1,27 @@
 <?php
 // Add custom Theme Functions here
+/**
+ * Add a custom product data tab
+ */
+add_filter( 'woocommerce_product_tabs', 'woo_new_product_tab' );
+function woo_new_product_tab( $tabs ) {
+	
+	// Adds the new tab
+	
+	$tabs['test_tab'] = array(
+		'title' 	=> __( 'Hướng Dẫn Mua Hàng', 'woocommerce' ),
+		'priority' 	=> 50,
+		'callback' 	=> 'woo_new_product_tab_content'
+	);
+
+	return $tabs;
+
+}
+function woo_new_product_tab_content() {
+
+	// The new tab content con
+
+	echo '<h2>New Product Tab</h2>';
+	echo '<p>Here\'s your new product tab.</p>';
+	
+}
